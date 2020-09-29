8efc4085552bf10e01d51bc57fa004e5eb71829a
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-09-29 20:30:53 +0530

    loginFunc

--- scan/views.py
+++ scan/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
-# Create your views here.
+from pymongo import MongoClient
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    client = MongoClient(
+        'mongodb+srv://abhip1912:abcd1234@cluster0.ywymp.mongodb.net/<dbname>?retryWrites=true&w=majority')
+    db = client.get_database('barcode')
+    orders = db.orders
+    users = db.users
+    mail = request.POST['email']
+    pw = request.POST['pass']
+
+    resp = list(users.find({'email': mail}))
+    if len(resp) > 0:
+        if resp[0]['pass'] == pw:
+            return render(request, 'home.html')
+        else:
+            return render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
