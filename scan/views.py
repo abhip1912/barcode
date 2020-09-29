98c19155762a7c7eee4acd6099be37986c719cae
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-09-29 20:35:16 +0530

    logInModified

--- scan/views.py
+++ scan/views.py
@@ -3,23 +3,22 @@ from pymongo import MongoClient
 
 
 def index(request):
-    return render(request, 'index.html')
+    if not request.method == 'POST':
+        return render(request, 'index.html')
+    else:
+        client = MongoClient(
+            'mongodb+srv://abhip1912:abcd1234@cluster0.ywymp.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        db = client.get_database('barcode')
+        orders = db.orders
+        users = db.users
+        mail = request.POST['email']
+        pw = request.POST['pass']
 
-
-def login(request):
-    client = MongoClient(
-        'mongodb+srv://abhip1912:abcd1234@cluster0.ywymp.mongodb.net/<dbname>?retryWrites=true&w=majority')
-    db = client.get_database('barcode')
-    orders = db.orders
-    users = db.users
-    mail = request.POST['email']
-    pw = request.POST['pass']
-
-    resp = list(users.find({'email': mail}))
-    if len(resp) > 0:
-        if resp[0]['pass'] == pw:
-            return render(request, 'home.html')
+        resp = list(users.find({'email': mail}))
+        if len(resp) > 0:
+            if resp[0]['pass'] == pw:
+                return render(request, 'home.html')
+            else:
+                return render(request, 'index.html')
         else:
             return render(request, 'index.html')
-    else:
-        return render(request, 'index.html')
