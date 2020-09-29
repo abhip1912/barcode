242ee32da61b1341babad4bf1ee623186e733e1b
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-09-29 22:41:24 +0530

    searchAdded

--- scan/views.py
+++ scan/views.py
@@ -11,15 +11,26 @@ def index(request):
         db = client.get_database('barcode')
         orders = db.orders
         users = db.users
-        mail = request.POST['email']
-        pw = request.POST['pass']
 
-        resp = list(users.find({'email': mail}))
-        if len(resp) > 0:
-            if resp[0]['pass'] == pw:
+        if 'orderid' in request.POST:
+            print(request.POST['orderid'])
+            if not request.POST['orderid']:
                 data = list(orders.find())
-                return render(request, 'home.html', {'data': data})
+            else:
+                data = list(orders.find({'orderId': request.POST['orderid']}))
+            return render(request, 'home.html', {'data': data})
+
+        else:
+            mail = request.POST['email']
+            pw = request.POST['pass']
+
+            resp = list(users.find({'email': mail}))
+
+            if len(resp) > 0:
+                if resp[0]['pass'] == pw:
+                    data = list(orders.find())
+                    return render(request, 'home.html', {'data': data})
+                else:
+                    return render(request, 'index.html')
             else:
                 return render(request, 'index.html')
-        else:
-            return render(request, 'index.html')
